100fa050432e748382a53bfe2dbd43d7c5dcd639
Benji1012 <55974827+Benji1012@users.noreply.github.com>
2025-05-05 07:00:41 +0200

    Spring boot default

---
+++ src/main/java/FantaziaSoftHungary/Twitch_foundry_backend/TwitchFoundryBackendApplication.java
@@ -0,0 +1,13 @@
+package FantaziaSoftHungary.Twitch_foundry_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchFoundryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchFoundryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FantaziaSoftHungary/Twitch_foundry_backend/TwitchFoundryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package FantaziaSoftHungary.Twitch_foundry_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchFoundryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
